8710ae283221e372dedbb7402b802cd0032d5de7
SamAziziDev <sadeghazizi.ca@gmail.com>
2023-05-08 20:36:31 -0700

    after fix buges and change user interfac for both views

--- src/main/java/com/codercampus/Assignment11/web/TransactionController.java
+++ src/main/java/com/codercampus/Assignment11/web/TransactionController.java
@@ -1,7 +1,5 @@
 package com.codercampus.Assignment11.web;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
